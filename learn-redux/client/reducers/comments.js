884469cb5c326365a039205e423f9de960392e7d
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-03-07 17:51:34 -0800

    finished the delete comment reducer.

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -7,12 +7,19 @@ function postComments(state = [], action) {
 				text: action.comment
 			}];
 		case 'REMOVE_COMMENT':
-			return state;
+			// we need to return the new state without the deleted comment
+			return [
+				// from the start to the one we want to delete
+				...state.slice(0, action.i),
+				//after the deleted one to end	
+				...state.slice(action.i + 1)
+			]
 		default:
 			return state;
 	}
 	return state;
 }
+
 function comments(state = [], action) {
 	if(typeof action.postId !== 'undefined') {
 		return {
