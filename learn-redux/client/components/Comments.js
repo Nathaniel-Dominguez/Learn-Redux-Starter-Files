95a1e19db8969a3602b8b96a935140ac2cf1d914
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-03-09 13:06:02 -0800

    fixed the add comment component.

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -17,6 +17,7 @@ const Comments = React.createClass({
 		const { postId } = this.props.params;
 		const author = this.refs.author.value;
 		const comment = this.refs.comment.value;
+		this.props.addComment(postId, author, comment);
 		this.refs.commentForm.reset();
 	},
 	render() {
